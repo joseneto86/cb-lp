e05f7e0a2a0ce37dcd7b1a21d742a2d7a19bb442
jose neto <jose.barbosaneto@dataprev.gov.br>
2025-07-17 15:07:14 -0300

    atualizacao

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.accordion .collapse').on('shown.bs.collapse', function () {
+    $(this).prev('.card-header').addClass('ativo');
+    $(this).prev().find('img').attr('src', 'img/arrow-up.png');
+  });
+
+  $('.accordion .collapse').on('hidden.bs.collapse', function () {
+    $(this).prev('.card-header').removeClass('ativo');
+    $(this).prev().find('img').attr('src', 'img/arrow-down.png');
+  });
+
+});
