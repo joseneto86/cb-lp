97ee6644df0dbcd9688646766b1e4a7209579a1a
jose neto <jose.neto86@gmail.com>
2025-07-28 12:05:07 -0300

    atulizacao

---
+++ charifa.php
@@ -0,0 +1,284 @@
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta
+        content="Chababy® Você merece o chá de bebê mais incrível. Crie sua lista de presentes, compartilhe com seus amigos e receba tudo em PIX de forma rápida, segura e TOTALMENTE GRÁTIS! A tradição continua, mas os presentes vão em um clique. Carregar fraldas ficou para trás."
+        name="keywords">
+    <meta
+        content="Chababy® Você merece o chá de bebê mais incrível. Crie sua lista de presentes, compartilhe com seus amigos e receba tudo em PIX de forma rápida, segura e TOTALMENTE GRÁTIS! A tradição continua, mas os presentes vão em um clique. Carregar fraldas ficou para trás."
+        name="description">
+
+    <!-- Favicon -->
+    <link href="/img/favicon.ico" rel="icon">
+    <title>CháBaby - Crie seu chá de bebê em poucos minutos</title>
+    <link rel="stylesheet" href="vendors/bootstrap-4.6.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendors/font-awesome/all.min.css">
+    <link rel="stylesheet" href="comum.css">
+    <link rel="stylesheet" href="charifa.css">
+    <link rel="stylesheet" href="sections.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Palanquin+Dark:wght@400&family=Palanquin:wght@400&display=swap">
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5GXSN4CW');
+    </script>
+    <!-- End Google Tag Manager -->
+</head>
+
+<body>
+    <section class="section1">
+        <div class="div-logo">
+            <img class="" src="img/logo-azul.webp" />
+        </div>
+
+        <div class="banner-container">
+            <img class="logo" src="img/logo-azul.webp" />
+            <div class="content-container">
+                <div class="main-title">
+                    <span class="title-text-black">Receba </span>
+                    <span class="title-text-purple">dinheiro </span>
+                    <span class="title-text-black">para o seu bebê e decida onde investir</span>
+                </div>
+                <div class="description">Com o Chá Rifa, isso é possível! Escolha um prêmio, defina os números e convide as pessoas para participarem. Quem ganha é você, um dos convidados e, é claro, o seu bebê!</div>
+                <div class="form-container">
+                    <div class="fields-container">
+                        <form action="https://chababy.com.br/registerlp" method="get">
+                            <div class="form-group w-100 mb-0">
+                                <input type="email" class="form-control" id="email" name="email"
+                                    placeholder="Digite seu e-mail">
+                            </div>
+                            <div class="form-group w-100 mb-0">
+                                <select class="form-control" id="semanas" name="semanas">
+                                    <option value="">Está grávida de quantas semanas? </option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    <option value="21">21</option>
+                                    <option value="22">22</option>
+                                    <option value="23">23</option>
+                                    <option value="24">24</option>
+                                    <option value="25">25</option>
+                                    <option value="26">26</option>
+                                    <option value="27">27</option>
+                                    <option value="28">28</option>
+                                    <option value="29">29</option>
+                                    <option value="30">30</option>
+                                    <option value="31">31</option>
+                                    <option value="32">32</option>
+                                    <option value="33">33</option>
+                                    <option value="34">34</option>
+                                    <option value="35">35</option>
+                                    <option value="36">36</option>
+                                    <option value="37">37</option>
+                                    <option value="38">38</option>
+                                    <option value="39">39</option>
+                                    <option value="40">40</option>
+                                    <option value="41">41</option>
+                                    <option value="42">42</option>
+                                </select>
+                            </div>
+                            <button id="acsubmit" class="action-button comecar-agora">COMEÇAR AGORA</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section2 container">
+        <div class="section2-container">
+
+            <div class="section2-content">
+                <div class="row">
+                    <div class="col-12 col-lg-6 col-md-7">
+                        <div class="section2-image-container">
+                            <img class="section2-image" src="img/charifa_section2.webp" />
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 col-md-5">
+                        <div class="section2-content-container">
+                            <div class="section2-content-text">
+                                <div class="section2-title">
+                                    <span class="section2-title-text-dark">Não espere mais para ter um</span>
+                                    <span class="section2-title-text-purple">Chá Rifa </span>
+                                    <span class="section2-title-text-dark">inesquecível!</span>
+                                </div>
+                                <div class="section2-description">
+                                    <p><b>O que é melhor do que receber presentes?</b> Simples, é escolher os presentes que você realmente precisa!</p>
+                                    <p>Com o Chá Rifa, você recebe <b>via Pix</b> e pode investir no que é realmente importante para o seu bebê: roupas, fraldas ou até mesmo cuidados médicos, tudo de forma prática e sem complicação.</p>
+                                </div>
+                                <button class="action-button comecar-agora action-click">COMEÇAR AGORA</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="section3">
+        <div class="container">
+            <div class="section3-header">
+                <div class="section3-title">
+                    <span class="section3-title-text-black">Ah, e o melhor é que você cria um  </span>
+                    <span class="section3-title-text-purple">site profissional em poucos minutos!</span>
+                </div>
+                <div class="section3-description">
+                    <span class="section3-description-text">Deixe tudo organizado em um só lugar, crie sua rifa, escolha o prêmio, compartilhe com os amigos e, o mais legal, personalize tudo com a sua carinha.</span>
+                </div>
+            </div>
+            <div class="section3-content">
+                <iframe src="https://www.youtube.com/embed/NYlC-SK4dL0?si=iwgIO7s88p4Gib3T" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <button class="action-button comecar-agora action-click">COMEÇAR AGORA</button>
+            </div>
+        </div>
+    </section>
+    <section class="section4">
+        <div class="container">
+            <div class="section4-header">
+                <div class="section4-title">
+                    <span class="section4-title-text-dark">Seu Chá Rifa ficou</span>
+                    <span class="section4-title-text-purple"> prático, moderno e na palma da sua mão.</span>
+                </div>
+            </div>
+            <div class="section4-content">
+                <div class="section4-steps-container ">
+                    <div class="section4-step section4-step-box">
+                        <div class="text-center"><img class="section4-step-image imgppt" src="img/icone1.webp" /></div>
+                        <div class="section4-step-title">Crie o seu site</div>
+                        <div class="section4-step-description">É rapidinho! Basta escolher a opção "Chá Rifa", ajustar os detalhes como bilhetes, valores, data do sorteio e o prêmio. Depois, é só personalizar com o tema que mais combina com você.<br /></div>
+                    </div>
+                   
+                    
+                    <div class="section4-step-2 section4-step-box">
+                        <img class="section4-step-image" src="img/charifa_icone2.webp" />
+                        <div class="section4-step-title">Envie o convite</div>
+                        <div class="section4-step-description">
+                            <span class="section4-step-description">Assim que seu site estiver pronto, basta enviar o convite digital para os convidados. Tudo fica muito mais simples! Eles escolhem os números disponíveis e já começam a torcer pelo sorteio. </span>
+                           
+
+                        </div>
+                    </div>
+                    
+                    <div class="section4-step-3 section4-step-box">
+                        <img class="section4-step-image" src="img/charifa_icone3.webp" />
+                        <div class="section4-step-3-title">Acompanhe em tempo real</div>
+                        <div class="section4-step-description">
+                            <span class="section4-step-description">Com sua rifa no ar, você acompanha tudo, vê quantos bilhetes foram vendidos, os números disponíveis e o valor arrecadado, direto do seu painel. </span>
+                           
+                        </div>
+                    </div>
+
+                    <div class="section4-step-3 section4-step-box">
+                        <div class="text-center"><img class="section4-step-image imgppt" src="img/charifa_icone4.webp" /></div>
+                        <div class="section4-step-3-title">Faça o sorteio</div>
+                        <div class="section4-step-description">
+                            <span class="section4-step-description">No dia marcado, basta acessar o site e clicar em "Sortear". O sistema seleciona um número aleatório automaticamente. E se quiser, você pode deixar o momento ainda mais especial com uma live no Instagram.</span>
+                        </div>
+                    </div>
+                </div>
+                <button class="action-button comecar-agora action-click">COMEÇAR AGORA</button>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="section5">
+        <div class="container">
+            <div class="section5-header">
+                <div class="section5-title">
+                    <span class="section5-title-text-black">Deixa com o </span>
+                    <span class="section5-title-text-purple">CháBaby!</span>
+                   
+                </div>
+            </div>
+            <div class="section5-content">
+                <div class="section5-image-container">
+                    <img class="section5-image" src="img/section5-img1.webp" />
+                </div>
+                <div class="section5-features">
+                    <div class="section5-features-container">
+                        <div class="section5-feature-card">
+                            <div class="section5-feature-header">
+                                <div class="section5-feature-icon"><img src="img/section5-ico_coracao.webp" /></div>
+                                <div class="section5-feature-title">Plataforma 100% gratuita</div>
+                            </div>
+                            <div class="section5-feature-description">Você usa sem pagar nada, porque a taxa já vem embutida no valor que o convidado escolhe.</div>
+                        </div>
+                        <div class="section5-feature-card">
+                            <div class="section5-feature-header">
+                                <div class="section5-feature-icon"><img src="img/section5-ico2.webp" /></div>
+                                <div class="section5-feature-title">Tudo pelo celular</div>
+                            </div>
+                            <div class="section5-feature-description">Monte e acompanhe seu Chá Rifa de forma rápida e prática, onde e quando quiser, pelo celular.</div>
+                        </div>
+                        <div class="section5-feature-card">
+                            <div class="section5-feature-header">
+                                <div class="section5-feature-icon"><img src="img/section5-ico_seguranca.webp" /></div>
+                                <div class="section5-feature-title">Segurança garantida</div>
+                            </div>
+                            <div class="section5-feature-description">Seu Chá Rifa acontece em um ambiente seguro e protegido, para você usar sem preocupações!</div>
+                        </div>
+                        <div class="section5-feature-card">
+                            <div class="section5-feature-header">
+                                <div class="section5-feature-icon"><img src="img/section5-ico_receber.webp" /></div>
+                                <div class="section5-feature-title">Presentes em dinheiro</div>
+                            </div>
+                            <div class="section5-feature-description">Receba os presentes em dinheiro, para usar como quiser.</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="w-100 text-center">
+                <button class="action-button comecar-agora action-click">COMEÇAR AGORA</button>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'sections.php'; ?>
+
+</body>
+<script src="vendors/jquery-3.6.0/jquery-3.6.0.min.js"></script>
+<script src="vendors/bootstrap-4.6.0-dist/js/bootstrap.bundle.min.js"></script>
+<script src="vendors/bootstrap-4.6.0-dist/js/bootstrap.min.js"></script>
+
+<script src="index.js"></script>
+
+</html>
